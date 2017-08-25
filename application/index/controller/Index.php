79a99054935117e85b95a41836a5eb2d6fe3b1a6
zfw203916 <15328628@qq.com>
2017-08-25 17:02:06 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -54,12 +54,12 @@ class Index extends Base
                         Loader::import('PHPMailer\SMTP');
                         $mail = new \PHPMailer();
                         $mail->IsSMTP();// 使用SMTP服务
-                        $mail->CharSet = "utf8";// 编码格式为utf8，不设置编码的话，中文会出现乱码
+                        $mail->CharSet = "utf8";
                         $mail->Host = "smtp.163.com";// 发送方的SMTP服务器地址
                         $mail->SMTPAuth = true;// 是否使用身份验证
-                        $mail->Username = "franktest@163.com";// 发送方的163邮箱用户名，就是你申请163的SMTP服务使用的163邮箱</span><span style="color:#333333;">
-                        $mail->Password = "frank203916";// 发送方的邮箱密码，注意用163邮箱这里填写的是“客户端授权密码”而不是邮箱的登录密码！</span><span style="color:#333333;">
-                        $mail->SMTPSecure = "ssl";// 使用ssl协议方式</span><span style="color:#333333;">
+                        $mail->Username = "franktest@163.com";// 发送方的163邮箱用户名
+                        $mail->Password = "frank203916";// “客户端授权密码”而不是邮箱的登录密码！
+                        $mail->SMTPSecure = "ssl";//ssl协议
                         $mail->Port = 994;// 163邮箱的ssl协议方式端口号是465/994
                         $mail->setFrom("franktest@163.com","franktest");// 设置发件人信息，如邮件格式说明中的发件人，这里会显示为Mailer(xxxx@163.com），Mailer是当做名字显示
                         $mail->addAddress($to,'11回复消息');// 设置收件人信息，如邮件格式说明中的收件人，这里会显示为Liang(yyyy@163.com)
@@ -71,7 +71,7 @@ class Index extends Base
                         $mail->Body = "以下是回复你的内容:".$desc_content."点击可以查看文章地址:".$desc_url;// 邮件正文
                         //$mail->AltBody = "This is the plain text纯文本";// 这个是设置纯文本方式显示的正文内容，如果不支持Html方式，就会用到这个，基本无用
 
-                        var_dump($mail->isSMTP());die;
+                        var_dump($mail->IsSMTP());die;
                         if(!$mail->send()){// 发送邮件
                                 return $mail->ErrorInfo;
                                 // echo "Message could not be sent.";
